54653f69f7a3844c6423197b6bfdfd5916ef0311
Le Khac Thanh Tung <lekhacthanhtung.it@gmail.com>
2024-01-12 11:28:15 +0700

    chore: setup base cw721 contract

--- base/cw721/examples/schema.rs
+++ base/cw721/examples/schema.rs
@@ -4,10 +4,10 @@ use std::fs::create_dir_all;
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
 use cw721::{
-    AllNftInfoResponse, ApprovedForAllResponse, ContractInfoResponse, NftInfoResponse,
-    NumTokensResponse, OwnerOfResponse, TokensResponse,
+    AllNftInfoResponse, ApprovedForAllResponse, ContractInfoResponse, Cw721HandleMsg,
+    Cw721QueryMsg, Cw721ReceiveMsg, NftInfoResponse, NumTokensResponse, OwnerOfResponse,
+    TokensResponse,
 };
-use orai_decal_nft::msg::{HandleMsg, InitMsg, QueryMsg, MigrateMsg};
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
@@ -15,15 +15,14 @@ fn main() {
     create_dir_all(&out_dir).unwrap();
     remove_schemas(&out_dir).unwrap();
 
-    export_schema(&schema_for!(InitMsg), &out_dir);
-    export_schema(&schema_for!(HandleMsg), &out_dir);
-    export_schema(&schema_for!(QueryMsg), &out_dir);
-    export_schema(&schema_for!(MigrateMsg), &out_dir);
+    export_schema(&schema_for!(Cw721HandleMsg), &out_dir);
+    export_schema(&schema_for!(Cw721QueryMsg), &out_dir);
+    export_schema(&schema_for!(Cw721ReceiveMsg), &out_dir);
     export_schema(&schema_for!(AllNftInfoResponse), &out_dir);
     export_schema(&schema_for!(ApprovedForAllResponse), &out_dir);
     export_schema(&schema_for!(ContractInfoResponse), &out_dir);
+    export_schema(&schema_for!(OwnerOfResponse), &out_dir);
     export_schema(&schema_for!(NftInfoResponse), &out_dir);
     export_schema(&schema_for!(NumTokensResponse), &out_dir);
-    export_schema(&schema_for!(OwnerOfResponse), &out_dir);
     export_schema(&schema_for!(TokensResponse), &out_dir);
 }
